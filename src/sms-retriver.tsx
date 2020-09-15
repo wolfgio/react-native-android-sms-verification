3eca13a4a56662980c568382c024b35b606326d2
Giovanny Piovesan <giowrp@gmail.com>
2020-09-15 18:16:34 -0300

    refactor: added condition to hook not crash if platform was not android

--- src/sms-retriver.tsx
+++ src/sms-retriver.tsx
@@ -2,9 +2,14 @@ import { useState, useRef, useCallback, useEffect, useMemo } from 'react';
 import { NativeModules, NativeEventEmitter, Platform } from 'react-native';
 import type { EmitterSubscription } from 'react-native';
 
-export default () => {
-  const AndroidSmsVerification =
-    Platform.OS === 'android' ? NativeModules.AndroidSmsVerification : null;
+interface UseSmsRetriver {
+  message: string;
+  startListener: () => void;
+  stopListener: () => void;
+}
+
+const useSmsRetriver = (): UseSmsRetriver => {
+  const { AndroidSmsVerification } = NativeModules;
   const [message, setMessage] = useState('');
   const listenerRef = useRef<EmitterSubscription>();
   const eventEmitter = useMemo(
@@ -13,42 +18,36 @@ export default () => {
   );
 
   useEffect(() => {
-    if (Platform.OS === 'android') {
-      const onStartListener = eventEmitter.addListener(
-        'onListenerStarted',
-        (event) => console.info(event)
-      );
-      const onRegisterListener = eventEmitter.addListener(
-        'onReceiverRegistered',
-        (event) => console.info(event)
-      );
-      const onUnregisterListener = eventEmitter.addListener(
-        'onReceiverUnregistered',
-        (event) => console.info(event)
-      );
+    const onStartListener = eventEmitter.addListener(
+      'onListenerStarted',
+      (event) => console.info(event)
+    );
+    const onRegisterListener = eventEmitter.addListener(
+      'onReceiverRegistered',
+      (event) => console.info(event)
+    );
+    const onUnregisterListener = eventEmitter.addListener(
+      'onReceiverUnregistered',
+      (event) => console.info(event)
+    );
 
-      return () => {
-        onStartListener?.remove();
-        onRegisterListener?.remove();
-        onUnregisterListener?.remove();
-      };
-    }
-
-    return () => null;
+    return () => {
+      onStartListener?.remove();
+      onRegisterListener?.remove();
+      onUnregisterListener?.remove();
+    };
   }, [eventEmitter]);
 
   const startListener = useCallback((): void => {
-    if (Platform.OS === 'android') {
-      try {
-        AndroidSmsVerification.registerBroadcastReceiver();
-        AndroidSmsVerification.startBroadcastReceiver();
-        listenerRef.current = eventEmitter.addListener(
-          'onMessageReceived',
-          setMessage
-        );
-      } catch (error) {
-        console.error(error);
-      }
+    try {
+      AndroidSmsVerification.registerBroadcastReceiver();
+      AndroidSmsVerification.startBroadcastReceiver();
+      listenerRef.current = eventEmitter.addListener(
+        'onMessageReceived',
+        setMessage
+      );
+    } catch (error) {
+      console.error(error);
     }
   }, [AndroidSmsVerification, eventEmitter]);
 
@@ -65,3 +64,12 @@ export default () => {
 
   return { message, startListener, stopListener };
 };
+
+export default Platform.OS === 'android'
+  ? useSmsRetriver
+  : () =>
+      ({
+        message: '',
+        startListener: () => {},
+        stopListener: () => {},
+      } as UseSmsRetriver);
